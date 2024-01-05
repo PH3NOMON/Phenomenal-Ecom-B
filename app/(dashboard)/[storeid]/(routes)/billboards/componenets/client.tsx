fdb3a30861235560d7506ea417ff5119ad78aa8f
Simon Shrestha <simonshrestha4@gmail.com>
2024-01-05 21:10:26 +0545

    billboards data table and more

--- app/(dashboard)/[storeid]/(routes)/billboards/componenets/client.tsx
+++ app/(dashboard)/[storeid]/(routes)/billboards/componenets/client.tsx
@@ -1,22 +1,28 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { CommandSeparator } from "@/components/ui/command";
-import { Heading } from "@/components/ui/heading";
-import { Separator } from "@/components/ui/separator";
 import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 
-export const BillboardClient = () => {
-  const router = useRouter();
+import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/ui/data-table";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { BillboardColumn, columns } from "./colums";
+// import { ApiList } from "@/components/ui/api-list";
+
+interface BillboardClientProps {
+  data: BillboardColumn[];
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
   const params = useParams();
+  const router = useRouter();
 
   return (
     <>
       <div className="flex items-center justify-between">
-        {" "}
         <Heading
-          title={`Billboards (0)`}
+          title={`Billboards (${data.length})`}
           description="Manage billboards for your store"
         />
         <Button
@@ -26,6 +32,10 @@ export const BillboardClient = () => {
         </Button>
       </div>
       <Separator />
+      <DataTable columns={columns} data={data} searchKey={"label"} />
+      <Heading title="API" description="API Calls for Billboards" />
+      <Separator />
+      {/* <ApiList entityName="billboards" entityIdName="billboardId" /> */}
     </>
   );
 };
