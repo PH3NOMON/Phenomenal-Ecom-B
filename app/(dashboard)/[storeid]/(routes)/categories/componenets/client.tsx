d5380a07d29ebe0c689da7ce8d1dcc4d31a74dfa
Simon Shrestha <simonshrestha4@gmail.com>
2024-01-08 18:46:38 +0545

    ram

--- app/(dashboard)/[storeid]/(routes)/categories/componenets/client.tsx
+++ app/(dashboard)/[storeid]/(routes)/categories/componenets/client.tsx
@@ -7,14 +7,15 @@ import { Button } from "@/components/ui/button";
 import { DataTable } from "@/components/ui/data-table";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
-import { BillboardColumn, columns } from "./colums";
+import { ApiAlert } from "@/components/ui/api-alert";
 import { ApiList } from "@/components/ui/api-list";
+import { columns, CategoryColumn } from "./colums";
 
-interface BillboardClientProps {
-  data: BillboardColumn[];
+interface CategoriesClientProps {
+  data: CategoryColumn[];
 }
 
-export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
+export const CategoriesClient: React.FC<CategoriesClientProps> = ({ data }) => {
   const params = useParams();
   const router = useRouter();
 
@@ -22,20 +23,20 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title={`Billboards (${data.length})`}
-          description="Manage billboards for your store"
+          title={`Categories (${data.length})`}
+          description="Manage categories for your store"
         />
         <Button
-          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+          onClick={() => router.push(`/${params.storeId}/categories/new`)}
         >
           <Plus className="mr-2 h-4 w-4" /> Add New
         </Button>
       </div>
       <Separator />
-      <DataTable columns={columns} data={data} searchKey={"label"} />
-      <Heading title="API" description="API Calls for Billboards" />
+      <DataTable searchKey="name" columns={columns} data={data} />
+      <Heading title="API" description="API Calls for Categories" />
       <Separator />
-      <ApiList entityName="billboards" entityIdName="billboardId" />
+      <ApiList entityName="categories" entityIdName="categoryId" />
     </>
   );
 };
