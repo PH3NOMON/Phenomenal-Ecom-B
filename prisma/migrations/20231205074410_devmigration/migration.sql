24184199883f5bd2ff83faaea9ffa4b04dd22db4
Simon Shrestha <simonshrestha4@gmail.com>
2024-01-10 11:29:16 +0545

    Sqlite to CockraodhDb

--- prisma/migrations/20231205074410_devmigration/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Store" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
